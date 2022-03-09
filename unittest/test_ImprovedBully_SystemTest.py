c81520568c4021e7eb219b28f98d362c158d5a15
Rikke <rikkefanoe@hotmail.com>
2022-03-09 15:29:26 +0100

    created 2 testfiles with correct names

--- unittest/test_ImprovedBully_SystemTest.py
+++ unittest/test_ImprovedBully_SystemTest.py
@@ -6,55 +6,7 @@ import wsnsimpy.wsnsimpy_tk as wsp
 import unittest   # The test framework
 
 
-class Test_test_improvedBully(unittest.TestCase):
-
-
-
-    def test_2nodes_start0(self):
-        clearMsgCount()
-        setElectionStarter(0)
-        sim = wsp.Simulator(
-            until=2,
-            timescale=1,
-            visual=True,
-            terrain_size=(SIZE,SIZE),
-            title="Improved Bully Election")
-        
-        nodeA = sim.add_node(MyNode, (100,100))
-        nodeA.tx_range = 2*SIZE
-        nodeA.logging = True
-
-        nodeB = sim.add_node(MyNode, (130,130))
-        nodeB.tx_range = 2*SIZE
-        nodeB.logging = True
-
-        sim.run()
-        self.assertEqual(nodeA.bestOK,nodeB.id)
-        print(f"message count  {getMsgCount()} \n")
-
-
-    def test_2nodes_start1(self):
-        clearMsgCount()
-        setElectionStarter(1)
-        sim = wsp.Simulator(
-            until=2,
-            timescale=1,
-            visual=True,
-            terrain_size=(SIZE,SIZE),
-            title="Improved Bully Election")
-        
-        nodeA = sim.add_node(MyNode, (100,100))
-        nodeA.tx_range = 2*SIZE
-        nodeA.logging = True
-
-        nodeB = sim.add_node(MyNode, (130,130))
-        nodeB.tx_range = 2*SIZE
-        nodeB.logging = True
-
-        sim.run()
-        self.assertEqual(nodeB.bestOK,nodeB.id)
-        print(f"message count  {getMsgCount()} \n")
-
+class Test_ImprovedBully_SystemTest(unittest.TestCase):
 
     def test_8nodes_starter0(self):
         clearMsgCount()
